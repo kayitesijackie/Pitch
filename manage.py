62a27bc51f4862e53c4751c84883fac10d30cf3c
kayitesijackie <bazimyas@gmail.com>
2019-02-25 16:10:47 +0200

    add app instance and define manager

--- manage.py
+++ manage.py
@@ -1,5 +1,11 @@
 from app import create_app,db
 from app.models import User
+from flask_script import Manager
+
+#Creating app instance
+app = create_app('development')
+
+manager = Manager(app)
 
 @manager.shell
 def make_shell_context():
