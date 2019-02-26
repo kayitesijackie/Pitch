a5d8367184aade39cc07e2fc214393322a03f17e
kayitesijackie <bazimyas@gmail.com>
2019-02-26 10:54:45 +0200

    property and password.setter decorators

--- app/models.py
+++ app/models.py
@@ -14,11 +14,23 @@ class User(UserMixin,db.Model):
     bio = db.Column(db.String(5000))
     profile_pic_path = db.Column(db.String)
     pass_secure = db.Column(db.String(255))
-    
+
+
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self,password):
+        self.pass_secure = generate_password_hash(password)
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
 
     def __repr__(self):
         return f'User {self.username}'
-
+    
 class Role(db.Model):
     __tablename__ = 'roles'
 
