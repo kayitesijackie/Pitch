68cc2130c2df5be66e65da4f9e0a80bfd6b6dae0
kayitesijackie <bazimyas@gmail.com>
2019-02-26 10:36:28 +0200

    import login_manager and datetime

--- app/models.py
+++ app/models.py
@@ -1,7 +1,8 @@
 from . import db
 from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin
-
+from . import login_manager
+from datetime import datetime
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key = True)
