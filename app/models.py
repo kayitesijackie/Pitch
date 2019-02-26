7a4d4e0c992e5b95144a54cd0cae65a85f52e6ee
kayitesijackie <bazimyas@gmail.com>
2019-02-26 14:52:34 +0200

    add login_manager.user_loader decorator

--- app/models.py
+++ app/models.py
@@ -3,6 +3,12 @@ from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin
 from . import login_manager
 from datetime import datetime
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
 class User(UserMixin,db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key = True)
@@ -11,7 +17,7 @@ class User(UserMixin,db.Model):
     firstname = db.Column(db.String(255))
     lastname = db.Column(db.String(255))
     email = db.Column(db.String(255),unique = True,index = True)
-    bio = db.Column(db.String(5000))
+    bio = db.Column(db.String(1000))
     profile_pic_path = db.Column(db.String)
     pass_secure = db.Column(db.String(255))
 
@@ -31,13 +37,5 @@ class User(UserMixin,db.Model):
     def __repr__(self):
         return f'User {self.username}'
     
-class Role(db.Model):
-    __tablename__ = 'roles'
 
-    id = db.Column(db.Integer,primary_key = True)
-    name = db.Column(db.String(255))
-    users = db.relationship('User',backref = 'role',lazy="dynamic")
-
-    def __repr__(self):
-        return f'User {self.name}'
 
