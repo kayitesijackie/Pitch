a79dbf5ec0d1d7bea8e575afe66b88ca6f24cd96
kayitesijackie <bazimyas@gmail.com>
2019-02-26 10:51:51 +0200

    add UserMixin instance in class user and six column in users table

--- app/models.py
+++ app/models.py
@@ -3,11 +3,18 @@ from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin
 from . import login_manager
 from datetime import datetime
-class User(db.Model):
+class User(UserMixin,db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
-    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    # role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    firstname = db.Column(db.String(255))
+    lastname = db.Column(db.String(255))
+    email = db.Column(db.String(255),unique = True,index = True)
+    bio = db.Column(db.String(5000))
+    profile_pic_path = db.Column(db.String)
+    pass_secure = db.Column(db.String(255))
+    
 
     def __repr__(self):
         return f'User {self.username}'
