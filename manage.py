247430665040b64f96014e29e160d80891d7bed5
kayitesijackie <bazimyas@gmail.com>
2019-02-26 10:25:38 +0200

    import Server, Migrate and MigrateCommand

--- manage.py
+++ manage.py
@@ -1,11 +1,14 @@
 from app import create_app,db
 from app.models import User
-from flask_script import Manager
+from flask_script import Manager, Server
+from flask_migrate import Migrate, MigrateCommand
 
 #Creating app instance
 app = create_app('development')
 
 manager = Manager(app)
+
+
 
 @manager.shell
 def make_shell_context():
