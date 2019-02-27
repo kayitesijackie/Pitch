5aac7d9ceb9580d88e461a341b5c6dd1448c6768
kayitesijackie <bazimyas@gmail.com>
2019-02-27 10:29:43 +0200

    change mail_message and change user instance

--- app/auth/views.py
+++ app/auth/views.py
@@ -7,7 +7,7 @@ from .. import db
 from ..email import mail_message
 
 
-@auth.route('/login', methods=['GET', 'POST'])
+@auth.route('/login',methods=['GET','POST'])
 def login():
     login_form = LoginForm()
     if login_form.validate_on_submit():
@@ -18,25 +18,25 @@ def login():
 
         flash('Invalid username or Password')
 
-    title = "Pitch App Login"
+    title = "One Minute Pitch login"
     return render_template('auth/login.html',login_form = login_form,title=title)
+
+@auth.route('/register',methods = ["GET","POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email = form.email.data, username = form.username.data,firstname= form.firstname.data,lastname= form.lastname.data,password = form.password.data)
+        db.session.add(user)
+        db.session.commit()
+
+        mail_message("Welcome to One Minute Pitch","email/welcome_user",user.email,user=user)
+
+        return redirect(url_for('auth.login'))
+        title = "New Account"
+    return render_template('auth/register.html',registration_form = form)
 
 @auth.route('/logout')
 @login_required
 def logout():
     logout_user()
     return redirect(url_for("main.index"))
-
-@auth.route('/register',methods = ["GET","POST"])
-def register():
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        user = User(email = form.email.data, username = form.username.data,password = form.password.data)
-        db.session.add(user)
-        db.session.commit()
-
-        mail_message("Welcome to the Pitch App","email/welcome_user",user.email,user=user)
-
-        return redirect(url_for('auth.login'))
-    title = "New Account"
-    return render_template('auth/register.html',registration_form = form)
